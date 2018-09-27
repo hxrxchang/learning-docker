0df9b00f94bb6ea4a949aeb4880d9834e10e5736
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-09-27 18:33:48 +0900

    add docker-compose

--- tutorial2/src/index.py
+++ tutorial2/src/index.py
@@ -3,6 +3,6 @@ from bottle import route, run
 @route('/')
 def hello():
     print('1111111111111111111111111111')
-    return "bottle is running!!"
+    return "docker is fun!!"
 
 run(host='0.0.0.0', port=8080, debug=True)
